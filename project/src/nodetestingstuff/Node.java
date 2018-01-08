793c801cdf212c06da31f9c003a85daf96d02020
Henry O'Range <Henri@DESKTOP-Q3CT6I1.fritz.box>
2018-01-08 23:19:07 +0100

    Starts implementing multiple antennas per Node still misses the prevention from connecting to the second antenna of a single Node

--- project/src/nodetestingstuff/Node.java
+++ project/src/nodetestingstuff/Node.java
@@ -12,6 +12,7 @@ public class Node extends Thread {
         
         // Adds an antenna
         this.antennas.add(new Antenna(this, 100, 0));
+        this.antennas.add(new Antenna(this, 100, 0));
     }
     
     @Override // Main thread of the node
@@ -25,53 +26,59 @@ public class Node extends Thread {
         for(int g = 0; g<antennas.size(); g++) {
             Antenna antenna = antennas.get(g);
             antenna.setPowerState(1);
-            ArrayList<AntennaSignal> signals = antennas.get(0).scanSignals();
-            Thread t = new Thread(() -> {
-                if (signals.size() == 0) {
-                    System.out.println("Server node opened up!");
 
-                    antenna.setMode(1);
-                    antenna.addAntennaEventListener((AntennaEvent evt) -> {
-                        if (evt.type.equals("onConnectionRequest")) {
-                            antenna.acceptConnectionRequest(evt.transmitterUUID);
-                        } else if (evt.type.equals("onClientConnect")) {
-                            System.out.println("Connected to a new client innit!");
-
-                            if (antenna.connectedClientUUIDs.size() > 5) antenna.setPowerState(0);
-                            if (antenna.connectedClientUUIDs.size() > 4) {
-                                for (int i = 0; i < antenna.connectedClientUUIDs.size(); i++) {
-                                    antenna.sendToClient(antenna.connectedClientUUIDs.get(i), "BOYS I AM FAMOUS!");
-                                }
-                            }
-                        } else if (evt.type.equals("onMessage")) {
-                            // TODO: Add message handling
-                        } else if (evt.type.equals("onDisconnect")) {
-                            System.out.println("Client disconnected! " + antenna.connectedClientUUIDs.size());
-                        }
-                    });
-                } else {
-                    antenna.addAntennaEventListener((AntennaEvent evt) -> {
-                        if (evt.type.equals("onMessage")) {
-                            System.out.println(evt.data);
-                        } else if (evt.type.equals("onDisconnect")) {
-                            System.out.println("Server disconnected!");
-                        } else if (evt.type.equals("onAccessPointConnect")) {
-                            // antenna.setPowerState(0);
-                        }
-                    });
-
-                    if(signals.size() > 1) {
-                        AntennaSignal transmitterAntenna = signals.get(0);
-                        for(int i = 1; i<signals.size(); i++) {
-                            if(signals.get(i).strength > transmitterAntenna.strength) {
-                                transmitterAntenna = signals.get(i);
-                            }
-                        }
-                        antenna.requestConnection(transmitterAntenna.transmitterUUID);
-                    }
-                    else antenna.requestConnection(signals.get(0).transmitterUUID);
+            ArrayList<AntennaSignal> signals = antennas.get(g).scanSignals();
+            for(int k = 0; k<signals.size(); k++) {
+                AntennaSignal signal = signals.get(k);
+                for(int l = 0; l<antennas.size(); l++) {
+                    if(signal.transmitterUUID.equals(antennas.get(l).UUID)) signals.remove(signal);
+                    if(signal.transmitterUUID.equals(antennas.get(l).connectedAccessPointUUID)) signals.remove(signal);
                 }
-            });
+            }
+            if (signals.size() == 0) {
+                System.out.println("Server node opened up!");
+
+                antenna.setMode(1);
+                antenna.addAntennaEventListener((AntennaEvent evt) -> {
+                    if (evt.type.equals("onConnectionRequest")) {
+                        antenna.acceptConnectionRequest(evt.transmitterUUID);
+                    } else if (evt.type.equals("onClientConnect")) {
+                        System.out.println("Connected to a new client innit!");
+
+                        if (antenna.connectedClientUUIDs.size() > 5) antenna.setPowerState(0);
+                        if (antenna.connectedClientUUIDs.size() > 4) {
+                            for (int i = 0; i < antenna.connectedClientUUIDs.size(); i++) {
+                                antenna.sendToClient(antenna.connectedClientUUIDs.get(i), "BOYS I AM FAMOUS!");
+                            }
+                        }
+                    } else if (evt.type.equals("onMessage")) {
+                        // TODO: Add message handling
+                    } else if (evt.type.equals("onDisconnect")) {
+                        System.out.println("Client disconnected! " + antenna.connectedClientUUIDs.size());
+                    }
+                });
+            } else {
+                antenna.addAntennaEventListener((AntennaEvent evt) -> {
+                    if (evt.type.equals("onMessage")) {
+                        System.out.println(evt.data);
+                    } else if (evt.type.equals("onDisconnect")) {
+                        System.out.println("Server disconnected!");
+                    } else if (evt.type.equals("onAccessPointConnect")) {
+                        // antenna.setPowerState(0);
+                    }
+                });
+
+                if(signals.size() > 1) {
+                    AntennaSignal transmitterAntenna = signals.get(0);
+                    for(int i = 1; i<signals.size(); i++) {
+                        if(signals.get(i).strength > transmitterAntenna.strength) {
+                            transmitterAntenna = signals.get(i);
+                        }
+                    }
+                    antenna.requestConnection(transmitterAntenna.transmitterUUID);
+                }
+                else antenna.requestConnection(signals.get(0).transmitterUUID);
+            }
         }
     }
     
